e97caae39272335df3476a10b98c6b4ea0c4c705
Rando Rommot <rando_rommot@live.com>
2017-02-05 00:28:34 +0200

    JavaScript test page added + minor fixes.

--- sketch.js
+++ sketch.js
@@ -1,7 +1,27 @@
 function setup() {
-  
+  var cnv = createCanvas(500, 500);
+  cnv.parent('sketch-holder');
+  background(112, 112, 112);
 }
 
 function draw() {
+  ellipseMode(CENTER);
+  rectMode(CENTER);
 
+  // Body
+  fill(255, 0, 100);
+  stroke(0, 0, 0);
+  rect(240, 145, 20, 100);
+
+  // Head
+  ellipse(240, 115, 60, 60);
+
+  // Eyes
+  fill(0, 255, 100);
+  ellipse(221, 115, 16, 32);
+  ellipse(259, 115, 16, 32);
+
+  // Legs
+  line(230, 195, 220, 205);
+  line(250, 195, 260, 205);
 }
